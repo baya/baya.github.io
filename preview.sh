61ea55ff65959669723cc2f48e2ea38eb59e92ba
jgm <kayak.jiang@gmail.com>
2016-03-26 18:02:29 +0800

    update

---
+++ preview.sh
@@ -0,0 +1,3 @@
+git ci -am 'update'
+git pull origin preview
+git push origin preview
---
+++ publish.sh
@@ -0,0 +1,8 @@
+git co preview
+git ci -am 'update'
+git pull origin preview
+git push origin preview
+git co master
+git merge preview
+git push origin master
+git co preview
